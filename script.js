13ff60b7acb1f55df5f84d084d075b129d7b253a
anamikanymous <anamikadash1707@gmail.com>
2026-06-30 19:05:14 +0530

    Phase 2 - Story transition

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
 /* ==========================
    Isha ❤️ Hritik
-   Phase 1 Script
+   Phase 2 Script
 ========================== */
 
 const heartsContainer = document.getElementById("hearts");
 
-/* Floating Hearts */
-
-function createHeart(){
+function createHeart() {
 
     const heart = document.createElement("div");
 
     heart.innerHTML = "🤍";
 
-   heart.style.position = "absolute";
-
-    heart.style.left = Math.random()*100 + "vw";
-
+    heart.style.position = "absolute";
+    heart.style.left = Math.random() * 100 + "vw";
     heart.style.top = "110vh";
-
-    heart.style.fontSize = (18 + Math.random()*18) + "px";
-
+    heart.style.fontSize = (18 + Math.random() * 18) + "px";
     heart.style.opacity = .7;
-
     heart.style.pointerEvents = "none";
-
-    heart.style.transition = "transform linear, opacity linear";
-
+    heart.style.transition = "transform 10s linear, opacity 10s linear";
     heart.style.zIndex = 1;
 
     heartsContainer.appendChild(heart);
 
-    const duration = 7000 + Math.random()*4000;
-
-    requestAnimationFrame(()=>{
+    requestAnimationFrame(() => {
 
         heart.style.transform =
-        `translateY(-130vh) rotate(${Math.random()*360}deg)`;
+            `translateY(-130vh) rotate(${Math.random() * 360}deg)`;
 
         heart.style.opacity = 0;
 
     });
 
-    setTimeout(()=>{
+    setTimeout(() => {
 
         heart.remove();
 
-    },duration);
+    }, 10000);
 
 }
 
-setInterval(createHeart,500);
+setInterval(createHeart, 500);
 
-/* Button */
 
-document.getElementById("startBtn").addEventListener("click",()=>{
+/* ---------- PAGE TRANSITIONS ---------- */
 
-    alert("✨ The surprise begins in Phase 2 ❤️");
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+
+document.getElementById("startBtn").addEventListener("click", () => {
+
+    page1.classList.add("fade-out");
+
+    setTimeout(() => {
+
+        page1.classList.add("hidden");
+
+        page2.classList.remove("hidden");
+        page2.classList.add("fade-in");
+
+    }, 600);
+
+});
+
+
+document.getElementById("continueBtn").addEventListener("click", () => {
+
+    alert("❤️ The big question comes in Phase 3 ❤️");
 
 });
